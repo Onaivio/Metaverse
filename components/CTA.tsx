9116cb3df9785f93d63ce025eb304d19cd0544b4
Onaivio <107544743+Onaivio@users.noreply.github.com>
2023-01-15 00:14:13 +0100

    ..

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -5,7 +5,7 @@ import Button from "./Button";
 const CTA = () => {
   return (
     <section
-      className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+      className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow mb-10`}
     >
       <div className="flex-1 flex flex-col">
         <h2 className={styles.heading2}>Let’s try our service now!</h2>
