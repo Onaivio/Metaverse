8721ddf8f2a0ac6c263ddfa836cef8961136d96d
Onaivio <107544743+Onaivio@users.noreply.github.com>
2023-01-16 13:59:50 +0100

    ..

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ import '../styles/globals.css'
 export default function Head() {
   return (
     <>
-      <title>Meta Messenger</title>
+      <title>HooBank | Decentralize Payment System</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </>
